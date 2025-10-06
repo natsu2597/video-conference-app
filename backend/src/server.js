5b20b9cbcec95ad351e0285f02afe5245e5cbfde
natsu2597 <atulrishi1997@gmail.com>
2025-10-06 11:08:35 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,35 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/clerk-sdk-node";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "@inngest/express";
 
 const app = express();
 
 const PORT = process.env.port || 5001;
+app.use(express.json()); // req.body
+app.use(clerkMiddleware()); // req.auth
+
+app.use("/api/inngest", serve({client : inngest, functions}))
 
 app.get("/", (req,res) => {
     res.send("Hello world");
 })
 
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port 5001      :http://localhost:${ENV.PORT}/`)
-})
+const startServer = async () => {
+    try{
+        await connectDB();
+        if(ENV.NODE_ENV !== "production") {
+            app.listen(ENV.PORT, () => {
+            console.log(`Server is running on port 5001      :http://localhost:${ENV.PORT}/`);
+            
+        })
+        }
+    }   
+    catch(error){
+        console.error("Error starting the sever", error);
+        process.exit(1);
+    }
+}
+
