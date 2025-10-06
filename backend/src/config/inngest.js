704c8526d20eaf0e171a6132f45c8ea677b56ebe
natsu2597 <atulrishi1997@gmail.com>
2025-10-06 19:43:00 +0530

    stream and sentry integration

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({id : "hiver" });
 
@@ -19,6 +20,13 @@ const syncUser = inngest.createFunction(
         }
 
         await User.create(newUser);
+
+        await upsertStreamUser({
+            id : newUser.clerkId.toString(),
+            name : newUser.name,
+            image : newUser.image,
+            email : newUser.email,
+        });
     }
 
 );
@@ -31,7 +39,7 @@ const deleteUserFromDB = inngest.createFunction(
         const { id } = event.data;
         await User.deleteOne({clerkId : id});
 
-        
+        await deleteStreamUser(id.toString());
     }
 )
 
