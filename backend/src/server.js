159954a9fdb1fc255894e9f7bf24ccb4f4a9db86
natsu2597 <atulrishi1997@gmail.com>
2025-10-06 11:37:48 +0530

    import error fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { functions, inngest } from "./config/inngest.js";
-import { serve } from "@inngest/express";
+import { serve } from "inngest/express";
 
 const app = express();
 
