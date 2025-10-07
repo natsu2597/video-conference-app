d3e19a591edc193a0f65d82fcadab1bea15ccd2b
natsu2597 <atulrishi1997@gmail.com>
2025-10-07 23:33:53 +0530

    Chat controller minor fix

--- backend/src/controller/chat.controller.js
+++ backend/src/controller/chat.controller.js
@@ -3,9 +3,8 @@ import { generateStreamToken } from "../config/stream.js"
 export const getStreamToken = async (req,res) => {
     try {
         const { user } = req.auth();
-        console.log(user);
         const token = generateStreamToken(req.auth().userId);
-        req.status(200).json({ token });
+        res.status(200).json({ token });
 
     } catch (error) {
         res.status(500).json({
